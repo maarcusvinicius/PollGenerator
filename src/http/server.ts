2ca0c4fabe8414cfd30271af05b0b48689cc8100
maarcusvinicius <marcus.editor77@gmail.com>
2024-02-15 20:49:57 -0300

    register pollResults and register websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
@@ -11,9 +14,12 @@ app.register(cookie, {
   hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
 });
 
+app.register(websocket)
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
