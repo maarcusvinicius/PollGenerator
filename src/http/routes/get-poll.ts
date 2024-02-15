8bdfbb0f33c06ba49d73732e5624b72b0ff0f9fd
maarcusvinicius <marcus.editor77@gmail.com>
2024-02-15 20:46:29 -0300

    adjuste vote channel

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
-import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+
+import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (req, reply) => {
@@ -24,6 +26,35 @@ export async function getPoll(app: FastifyInstance) {
       }
     });
 
-    return reply.status(200).send({ poll });
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found" });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+    // console.log(result);
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 == 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, {[line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    return reply.status(200).send({ 
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: (option.id in votes) ? votes[option.id] : 0
+          }
+        })
+      }
+     });
   });
 }
