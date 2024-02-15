1001680955e9140f27edda7d9029c3ebf2cd3e53
maarcusvinicius <marcus.editor77@gmail.com>
2024-02-15 20:47:38 -0300

    add websocket route pollId/results

---
+++ src/http/ws/poll-results.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+import { FastifyInstance } from "fastify";
+
+import { voting } from "../../utils/voting-pub-sub";
+
+export async function pollResults(app: FastifyInstance) {
+  app.get("/polls/:pollId/results", { websocket: true }, (connection, req) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid()
+    });
+
+    const { pollId } = getPollParams.parse(req.params);
+
+    voting.subscribe(pollId, (message) => {
+      connection.socket.send(JSON.stringify(message))
+    })
+  });
+}
